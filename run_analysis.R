c2c90b7957f004ce89722396e77cbcd39391ca67
Ellaqyzeng <46781793+Ellaqyzeng@users.noreply.github.com>
2020-09-02 18:38:18 +0100

    Update run_analysis.R

--- run_analysis.R
+++ run_analysis.R
@@ -1,9 +1,52 @@
 # Getting and Cleaning Data Project John Hopkins Coursera
 
 # This R code should do the following works:
-## Merges the training and the test sets to create one data set.
-## Extracts only the measurements on the mean and standard deviation for each measurement.
-## Uses descriptive activity names to name the activities in the data set
-## Appropriately labels the data set with descriptive variable names.
-## From the data set in step 4, creates a second, independent tidy data set with the average of each variable for each activity and each subject.
+# 1. Merges the training and the test sets to create one data set.
+# 2. Extracts only the measurements on the mean and standard deviation for each measurement.
+# 3. Uses descriptive activity names to name the activities in the data set
+# 4. Appropriately labels the data set with descriptive variable names.
+# 5. From the data set in step 4, creates a second, independent tidy data set with the average of each variable for each activity and each subject.
 
+
+library(reshape2)
+
+#reading features and activity data
+features <- read.table("./UCI HAR Dataset/features.txt")
+activities <- read.table("./UCI HAR Dataset/activity_labels.txt")
+
+#reading train data
+train <- read.table("./UCI HAR Dataset/train/X_train.txt") #features data
+colnames(train) <- features$V2 #descriptive column names for data (STEP 4)
+y_train <- read.table("./UCI HAR Dataset/train/y_train.txt") #activity labels
+train$activity <- y_train$V1
+subject_train <- read.table("./UCI HAR Dataset/train/subject_train.txt") #subjects
+train$subject <- factor(subject_train$V1)
+
+
+#reading test data
+test <- read.table("./UCI HAR Dataset/test/X_test.txt")
+colnames(test) <- features$V2
+y_test <- read.table("./UCI HAR Dataset/test/y_test.txt") 
+test$activity <- y_test$V1
+subject_test <- read.table("./UCI HAR Dataset/test/subject_test.txt")
+test$subject <- factor(subject_test$V1)
+
+#merge train and test sets (STEP 1)
+dataset <- rbind(test, train) 
+
+#filter column names (STEP 2)
+column.names <- colnames(dataset)
+#get only columns for standard deviation and mean values, also saves activity and subject values 
+column.names.filtered <- grep("std\\(\\)|mean\\(\\)|activity|subject", column.names, value=TRUE)
+dataset.filtered <- dataset[, column.names.filtered] 
+
+#adding descriptive values for activity labels (STEP 3)
+dataset.filtered$activitylabel <- factor(dataset.filtered$activity, labels= c("WALKING", "WALKING_UPSTAIRS", "WALKING_DOWNSTAIRS", "SITTING", "STANDING", "LAYING"))
+
+#creating a tidy dataset with mean values for each subject and activity
+features.colnames = grep("std\\(\\)|mean\\(\\)", column.names, value=TRUE)
+dataset.melt <-melt(dataset.filtered, id = c('activitylabel', 'subject'), measure.vars = features.colnames)
+dataset.tidy <- dcast(dataset.melt, activitylabel + subject ~ variable, mean)
+
+#creating a tidy dataset file  
+write.table(dataset.tidy, file = "tidydataset.txt", row.names = FALSE)
